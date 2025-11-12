0cb1e279c5fcc5c2988d3b698a26433c6eac47a1
Kshitij <vermakshitij19@gmail.com>
2025-11-12 11:16:31 +0530

    main code

---
+++ app.py
@@ -0,0 +1,16 @@
+import random
+
+secret_number = random.randint(1, 10)
+attempts = 0
+
+while True:
+    guess = int(input("Guess a number between 1 and 10: "))
+    attempts += 1
+
+    if guess == secret_number:
+        print(f"Congratulations! You guessed the number in {attempts} attempts.")
+        break
+    elif guess < secret_number:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again."
